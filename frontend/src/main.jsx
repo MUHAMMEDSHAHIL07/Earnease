b5aa2e2c9c2a792ce118a8446a23c023e61861b7
MUHAMMEDSHAHIL07 <161433768+MUHAMMEDSHAHIL07@users.noreply.github.com>
2025-09-18 21:32:59 +0530

    fix:changes in main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { GoogleOAuthProvider } from '@react-oauth/google'
-import './index.css'
-import App from './App.jsx'
-import { AuthProvider } from "./context/AuthContext"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import { AuthProvider } from "./context/AuthContext";
 
 const CLIENTID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-createRoot(document.getElementById('root')).render(
 
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={CLIENTID}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </GoogleOAuthProvider>
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId={CLIENTID}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </GoogleOAuthProvider>
+    </BrowserRouter>
   </StrictMode>
-)
+);
