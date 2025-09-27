ea5858b788400f6db4ac1ea4be00d9c44a12b997
MUHAMMEDSHAHIL07 <161433768+MUHAMMEDSHAHIL07@users.noreply.github.com>
2025-09-27 13:59:14 +0530

    fix: Prevent 401 error on public pages

--- frontend/src/context/authContext.jsx
+++ frontend/src/context/authContext.jsx
@@ -1,18 +1,18 @@
-import { createContext, useContext, useEffect, useState } from "react"
-import axios from "axios"
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
 
-const AuthContext = createContext(null)
+const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
-  const [globalLoading, setGlobalLoading] = useState(true)
-
+  const [user, setUser] = useState(null);
+  const [globalLoading, setGlobalLoading] = useState(true);
   const fetchUser = async () => {
     try {
-      const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/auth/me`, { withCredentials: true })
-      setUser(res.data.user)
+      const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/auth/me`, { withCredentials: true });
+      setUser(res.data.user);
 
       if (res.data.user) {
+        localStorage.setItem('isLoggedIn', 'true'); 
         localStorage.setItem(
           "earneaseUser",
           JSON.stringify({
@@ -21,19 +21,27 @@ export const AuthProvider = ({ children }) => {
           })
         );
       } else {
-        localStorage.removeItem("earneaseUser")
+        localStorage.removeItem("earneaseUser");
+        localStorage.removeItem('isLoggedIn'); 
       }
     } catch (err) {
       setUser(null);
-      localStorage.removeItem("earneaseUser")
+      localStorage.removeItem("earneaseUser");
+      localStorage.removeItem('isLoggedIn'); 
     } finally {
-      setGlobalLoading(false)
+      setGlobalLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchUser()
+    const loggedInFlag = localStorage.getItem('isLoggedIn');
+    if (loggedInFlag === 'true') {
+      fetchUser();
+    } else {
+      setGlobalLoading(false);
+    }
   }, []);
+
 
   return (
     <AuthContext.Provider value={{ user, setUser, globalLoading, setGlobalLoading, fetchUser }}>
@@ -42,4 +50,4 @@ export const AuthProvider = ({ children }) => {
   );
 };
 
-export const useAuth = () => useContext(AuthContext)
+export const useAuth = () => useContext(AuthContext);
