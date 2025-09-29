0e32f30f8be292ffa77f81cf222f5c4e567e2097
MUHAMMEDSHAHIL07 <161433768+MUHAMMEDSHAHIL07@users.noreply.github.com>
2025-09-29 11:16:03 +0530

    fix:chat controller changes

--- backend/src/socket/chatMessageController.js
+++ backend/src/socket/chatMessageController.js
@@ -1,6 +1,5 @@
 import { messageModel } from "../models/message.js";
 import { chatRoomModel } from "../models/chatRoom.js";
-import mongoose from "mongoose";
 
 let io;
 export const setSocketIO = (ioInstance) => {
@@ -8,30 +7,25 @@ export const setSocketIO = (ioInstance) => {
 };
 
 export const getMessage = async (req, res) => {
-    const { chatRoomId } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(chatRoomId)) {
-        return res.status(400).json({ message: 'Invalid chat room ID format' });
-    } 
     try {
-        const chatRoom = await chatRoomModel.findById(chatRoomId);
-        if (!chatRoom) {
-            return res.status(404).json({ message: "Chat not found" });
-        }
+        const chatRoom = await chatRoomModel.findById(req.params.chatRoomId);
+        if (!chatRoom) return res.status(404).json({ message: "Chat not found" });
+
         if (
             String(chatRoom.student) !== req.user.id &&
             String(chatRoom.employer) !== req.user.id
         ) {
             return res.status(403).json({ message: "Not authorized to access this chat" });
         }
+
         const messages = await messageModel
-            .find({ chatRoom: chatRoomId })
-            .populate("sender", "name avatarUrl")
+            .find({ chatRoom: chatRoom._id })
             .sort({ createdAt: 1 });
 
         res.json({ success: true, data: messages });
+        console.log(messages)
     } catch (err) {
-        console.error("Error in getMessage controller:", err);
-        res.status(500).json(err.message);
+        res.status(500).json({ message: err.message });
     }
 };
 
@@ -48,9 +42,8 @@ export const sendMessage = async (req, res) => {
             senderRole: req.user.role,
             text,
         })
-        const populatedMessage = await newMessage.populate("sender", "name avatarUrl")
         if (io) {
-            io.to(chatRoomId).emit("receiveMessage", populatedMessage);
+            io.to(chatRoomId).emit("receiveMessage", newMessage);
         }
 
         res.json({ success: true, data: newMessage });
