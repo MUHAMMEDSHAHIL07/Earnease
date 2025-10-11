5b40fd17c34a64754bf3887f199bee825e912f16
MUHAMMEDSHAHIL07 <161433768+MUHAMMEDSHAHIL07@users.noreply.github.com>
2025-10-11 19:51:05 +0530

    new commit

--- backend/src/controllers/auth/sendOtp.js
+++ backend/src/controllers/auth/sendOtp.js
@@ -5,15 +5,15 @@ import { sendEmail } from '../../utils/sendEmail.js';
 export const sendOtp = async (req, res) => {
   const { email } = req.body;
   
-  await otpModel.deleteMany({ email });
+  await otpModel.deleteMany({ email })
 
   const otp = otpGenerator.generate(6, {
     digits: true, upperCaseAlphabets: false,
     lowerCaseAlphabets: false, specialChars: false,
-  });
+  })
 
-  await otpModel.create({ email, otp });
-  await sendEmail(email, otp);
+  await otpModel.create({ email, otp })
+  await sendEmail(email, otp)
 
-  return res.status(200).json({ message: 'OTP sent' });
-};
+  return res.status(200).json({ message: 'OTP sent' })
+}
